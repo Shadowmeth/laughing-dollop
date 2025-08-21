9a36c9303243e98a3667ba6eb5a15ae1aadd852d
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-08-21 19:47:48 +0500

    Routes now send files instead of hardcoded html

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,15 @@
 const express = require("express");
+const path = require("node:path");
 
 const router = express.Router();
 
 router.get("/add-product", (_req, res, _next) => {
-  res.send(
-    `<form action="/admin/add-product" method="POST"> ` +
-      `<input type="text" name="title">` +
-      `<button type="submit">Add Product</button`,
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, _next) => {
+  // note that the actual handling of data will be done here
+  // for example saving the file into a database
   console.log(req.body);
   res.redirect("/");
 });
