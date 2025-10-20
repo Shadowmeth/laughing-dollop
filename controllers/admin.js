e7015b55be6e6ed4f7328a2fcc719da1abbcb8bc
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-10-20 17:15:41 +0500

    Enhanced the app

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 
-exports.getAddProduct = (_req, res, _next) => {
+exports.getAddProduct = (req, res, next) => {
   res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
@@ -10,13 +10,17 @@ exports.getAddProduct = (_req, res, _next) => {
   });
 };
 
-exports.postAddProduct = (req, res, _next) => {
-  const product = new Product(req.body.title);
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
 
-exports.getProducts = (_req, res, _next) => {
+exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
       prods: products,
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 
-exports.getProducts = (_req, res, _next) => {
+exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
       prods: products,
@@ -20,14 +20,21 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
-exports.getCart = (_req, res, _next) => {
+exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
     pageTitle: "Your Cart",
   });
 };
 
-exports.getCheckout = (_req, res, _next) => {
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
     pageTitle: "Checkout",
